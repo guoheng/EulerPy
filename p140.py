bfad97aa809eecd0406c1141ccc61d424d2f26a7
Wei Guoheng <wei.guoheng@intellif.com>
2019-02-05 22:42:22 -0600

    fixed p140,141

--- p140.py
+++ p140.py
@@ -8,8 +8,6 @@ from math import sqrt
 import logging
 logger = logging.getLogger('p140')
 
-from prime import PrimeNumberPool
-
 # Analysis
 # problem can be reduced to find integer n such that
 # (n+3)x^2+(n+1)x-n = 0 has rational solution
@@ -23,21 +21,30 @@ def IsSqrt(n):
 def main(args):
 
     if args.test:
-        N = 18
+        N = 20
     else:
         N = 30
 
-    primes = PrimeNumberPool()
-
     golden_nuggets = []
+    golden_m = []
 
-    n = 1
+    # n = 21k + 0,2,5
+    k = 0
     while len(golden_nuggets) < N:
-        d = (n+1)*(n+1)+4*n*(n+3)
-        if IsSqrt(d):
-            golden_nuggets.append(n)
-            logger.debug("n={}, m={}".format(n, int(sqrt(d))))
-        n += 1
+        for r in [0, 2, 5]:
+            n = 21*k + r
+            if n == 0:
+                continue
+            d = (n+1)*(n+1)+4*n*(n+3)
+            m = int(sqrt(d))
+            if m*m == d:
+                golden_nuggets.append(n)
+                golden_m.append(m)
+                logger.debug("n={}, m={}, k={}".format(n, m, k))
+                if len(golden_m) > 3:
+                    k = int(golden_m[-4]*1.00045) - 1
+                    logger.debug("jump k to {}".format(k))
+        k += 1
 
     logger.debug(golden_nuggets)
 
--- p141.py
+++ p141.py
@@ -13,21 +13,43 @@
 
 # Find the sum of all progressive perfect squares below one trillion (1012).
 
+import math
+from util import GCD
 import logging
 logger = logging.getLogger('p141')
 
 # Analysis
 #
+# let r = ab^2, d = abc, q = ac^2, where b < c
+# n^2 = r + dq = ab^2 + a^2bc^3
+#
 
+def cube_root(x):
+    return math.pow(x, 1/3)
 
 def main(args):
-
     K = 1000
     if args.test:
-        N = 100*K
+        N = 1000*K
     else:
-        N = K*K*K*KeyboardInterrupt
+        N = K*K*K*K
 
+    C = int(cube_root(N))
+    progressive_perfect_squares = []
 
-    answer = 0
+    for c in range(2, C+1):
+        c3 = c*c*c
+        B = min(N//c3, c)
+        for b in range(1, B):
+            if GCD(b,c) > 1:
+                continue
+            A = int(math.sqrt(N//c3//b))
+            for a in range(1, A):
+                n2 = a*b*(b+a*c3)
+                n = int(math.sqrt(n2))
+                if n*n == n2:
+                    progressive_perfect_squares.append(n2)
+                    logger.debug("{} = {}^2 = {} + {}*{}".format(n2, n, a*b*b, a*b*c, a*c*c))
+
+    answer = sum(list(set(progressive_perfect_squares)))
     logger.info("answer: {}".format(answer))
